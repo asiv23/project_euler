cf449649ed2806eee12d3913a2669383ca556e3f
Baz Siv <ansivana@gmail.com>
2023-05-31 16:03:53 +0700

    Finished answers for problems 1 and 2

---
+++ p001.py
@@ -0,0 +1,31 @@
+'''
+Problmen 1 - Multiples of 3 or 5
+
+This program finds the sum of multiples from two numbers until an upper limit.
+If a number is a multiple of both inputs they're only counted once.
+Originally the program was hardcoded for the numbers given in the problem but it has been modified
+so that user will input the two numbers, then an upper limit.
+'''
+
+def getMultipleSum(x, y, limit): #get multiples of x (user input) from x to limit
+    sum = 0
+    for num in range(0, limit): #for loop to find multiples
+        if (num % x == 0 or num % y == 0): #if mod of num is 0, add to array of multiples
+            sum = sum + num
+    
+    return sum
+
+def main():
+    print("This program finds the sum of multiples from two numbers until an upper limit.")
+    firstnum = int(input("Enter your first number: "))# Get user input - number
+    secondnum = int(input("Enter your second number: "))# Get user input - number
+    limit = int(input("Enter the limit: "))# Get user inpun - limit
+    
+    totalSum = getMultipleSum(firstnum, secondnum, limit)
+
+    print()
+    print("The sum of the multiples of {} and {} is {}" .format(firstnum, secondnum, totalSum))
+
+if __name__ == "__main__":
+    main()
+
---
+++ p002.py
@@ -0,0 +1,65 @@
+'''
+Problem 2 - Even Fibonacci Numbers
+
+This program finds the sum of the even or odd numbers of the Fibonacci sequence starting from 1,2 until an upper limit.
+Originally the program was hardcoded to print the sum of even numbers under 4 million
+but has been updated to accomidate any limit as well as summing odd or even numbers.
+Error checking implemented on user input of parrity, must enter 1 for odd or 2 for even, repeats until recieves valid answer
+'''
+def Fibonacci(limit): #finds number in Fibonacci sequence until limit inputted by user
+    fib = [1, 2] #starting squence
+    x = 0 #variable for next number in sequence
+    num = 2 #counter
+
+    while(x <= limit):
+        if (num == 2):
+            x = fib[0] + fib[1]#base case
+        else:
+            x = fib[num-2] + fib[num-1]
+        
+        if x > limit:
+            break
+
+        fib.append(x)
+        num = num + 1
+    
+    return fib
+
+def findEvenSum(list):
+    listSum = 0
+    for num in range(len(list)):
+        if (list[num] % 2 == 0):
+            listSum = listSum + list[num]
+    
+    return listSum
+
+def findOddSum(list):
+    listSum = 0
+    for num in range(len(list)):
+        if (list[num] % 2 != 0):
+            listSum = listSum + list[num]
+    
+    return listSum
+
+def main():
+    print("This program finds the sum of the even or odd numbers of the Fibonacci sequence starting from 1,2 until an upper limit.")
+    limit = int(input("Enter the limit: "))# Get user input - limit
+    fib = Fibonacci(limit)
+
+    error = True
+    while (error == True):
+        paritynum = int(input("Enter the parity, 1 for odd or 2 for even: "))# Get user input
+        if paritynum == 1:
+            parity = "even"
+            totalsum = findEvenSum(fib)
+            break
+        elif paritynum == 2:
+            parity = "odd"
+            totalsum = findOddSum(fib)
+            break
+        print("The answer you've inputted is invalid, please try again")
+    
+    print("The total sum of the {} numbers in the Fibonacci sequence under {} is {}" .format(parity, limit, totalsum))
+
+if __name__ == "__main__":
+    main()
